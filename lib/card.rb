4e15e5a9d4c5f6d1e1ce0ef543add50cecb436cf
dominicmacaulay <dominic.62502@gmail.com>
2024-06-12 16:48:47 -0400

    Write syntax to pass card tests

---
+++ lib/card.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# playing card class
+class Card
+  attr_reader :rank, :suit
+
+  RANKS = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace].freeze
+  SUITS = %w[Spades Clubs Hearts Diamonds].freeze
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def value
+    @value ||= RANKS.index(rank)
+  end
+end
