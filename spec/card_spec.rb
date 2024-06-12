c4a34db59224124fba5a35c51eb04faa8886cf95
dominicmacaulay <dominic.62502@gmail.com>
2024-06-12 16:51:43 -0400

    Write tests for card.equal_rank? for easy card comparison

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe Card do
       expect(card.value).to eql index_of_number_four
     end
   end
+
+  describe 'equal_rank?' do
+    it 'should return true if the ranks are equal' do
+      other_card = Card.new('4', 'Spades')
+      expect(card.equal_rank?(other_card)).to be true
+    end
+    it 'should return false if the ranks are not equal' do
+      other_card = Card.new('5', 'Hearts')
+      expect(card.equal_rank?(other_card)).to be false
+    end
+  end
 end
