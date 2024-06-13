0e88f90bfa1b4569c4b297af6a6d547ea93f7910
dominicmacaulay <dominic.62502@gmail.com>
2024-06-13 09:19:19 -0400

    Write test for player.add_to_hand with implicit player.hand_count

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative 'spec_helper'
 require_relative '../lib/player'
+require_relative '../lib/card'
 
 RSpec.describe Player do
   describe 'initialize' do
@@ -10,6 +11,17 @@ RSpec.describe Player do
       player2 = Player.new('Josh')
       expect(player1.name).to eql 'Dom'
       expect(player2.name).to eql 'Josh'
+    end
+  end
+
+  describe 'add_to_hand' do
+    it "should add the given cards to the player's hand" do
+      player = Player.new('Dom')
+      card1 = Card.new('4', 'Hearts')
+      card2 = Card.new('5', 'Spades')
+      player.add_to_hand([card1, card2])
+      expect(player.hand_count).to be 2
+      expect(player.hand).to include(card1, card2)
     end
   end
 end
