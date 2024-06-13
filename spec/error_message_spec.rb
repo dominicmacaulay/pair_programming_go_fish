79377b99115a3d5179464a7af50bbf038bb650c2
dominicmacaulay <dominic.62502@gmail.com>
2024-06-13 10:01:20 -0400

    Write tests for ErrorMessage.==

--- spec/error_message_spec.rb
+++ spec/error_message_spec.rb
@@ -4,6 +4,19 @@ require_relative 'spec_helper'
 require_relative '../lib/error_message'
 
 RSpec.describe ErrorMessage do
+  describe '==' do
+    it 'returns true if all attributes are equal' do
+      message1 = ErrorMessage.new
+      message2 = ErrorMessage.new
+      expect(message1).to eq message2
+    end
+    it 'returns false in any attributes are not equal' do
+      message1 = ErrorMessage.new(name: 'Josh')
+      message2 = ErrorMessage.new
+      expect(message1).not_to eq message2
+    end
+  end
+
   describe 'name error message' do
     it 'returns a string indicating the given name is invalid' do
       name = 'Micah'
